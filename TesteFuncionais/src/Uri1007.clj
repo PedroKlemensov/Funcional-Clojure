48f9c242a32dd4d0a38c7af60aa75f2bd4ccb246
Pedroklemensov <80336621+PedroKlemensov@users.noreply.github.com>
2023-02-13 19:36:32 -0300

    a

---
+++ TesteFuncionais/src/Uri1007.clj
@@ -0,0 +1,8 @@
+(ns Uri1007)
+(def valora (read-string (read-line)))
+(def valorb (read-string (read-line)))
+(def valorc (read-string (read-line)))
+(def valord (read-string (read-line)))
+
+(def difererança (- (* valora valorb) (* valorc valord)))
+(println "DIFERENCA =" difererança)
