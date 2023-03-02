1589183c93aa7d79096e0b62d3f7750101874a3f
Pedroklemensov <pedroklemensov@gmail.com>
2023-03-01 21:49:56 -0300

    j

--- TesteFuncionais/src/Uri1035.clj
+++ TesteFuncionais/src/Uri1035.clj
@@ -1,1 +1,10 @@
 (ns Uri1035)
+(require '[clojure.string :as str])
+
+(def entrada (str/split (read-line) #" "))
+(def a (read-string (get entrada 0)))
+(def b (read-string (get entrada 1)))
+(def c (read-string (get entrada 2)))
+(def d (read-string (get entrada 3)))
+
+(c)
