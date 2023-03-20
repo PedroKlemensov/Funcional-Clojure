c7ccd4b0912ce57c7d2cbf4832fb123382555f5f
Pedroklemensov <pedroklemensov@gmail.com>
2023-03-19 23:15:44 -0300

    a

--- TesteFuncionais/src/Uri1048.clj
+++ TesteFuncionais/src/Uri1048.clj
@@ -1,13 +1,15 @@
 (ns Uri1048)
 (def salario (read-string (read-line)))
 
-(defn novosalario [salario porcento]
+(defn novosalario [porcento]
 
   (* salario (+ 1 (/ porcento 100.0)))
   )
 
+;(format "%.2f\n%.2f" (novosalario 15) (- (novosalario 15) salario) )
+
 (println (cond
-
-
+           (>= 400 salario) (format "Novo salario: %.2f \nReajuste ganho: %.2f \nEm percentual: 15 %" 460.000 60.000
+                                    )
 
            ))
