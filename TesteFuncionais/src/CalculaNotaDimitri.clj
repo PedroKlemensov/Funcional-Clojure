ce3a836ac21f9e93253101bfd360a6fe58c2caae
Pedroklemensov <80336621+PedroKlemensov@users.noreply.github.com>
2023-04-12 07:55:28 -0300

    Update CalculaNotaDimitri.clj

--- TesteFuncionais/src/CalculaNotaDimitri.clj
+++ TesteFuncionais/src/CalculaNotaDimitri.clj
@@ -6,9 +6,9 @@
                   5) )
 
 
-  (println cadeira ":" )
-  (println (+ (* ap 2) (* ab 2) ntd))
-  (print (format  "%.2f" calculo))
+  ;(println cadeira ":" )
+  ;(println (+ (* ap 2) (* ab 2) ntd))
+  (print (format  "%s: %.2f" cadeira calculo))
   )
 
-(calculamedia "ciencias" 8.3 3.5 9.3)
+(calculamedia "ciencias" 8.5 3.5 9.3)
