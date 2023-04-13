8e71538e1c41ce3f6bdf56c27d1659390bc910ab
Pedroklemensov <pedroklemensov@gmail.com>
2023-04-12 22:23:33 -0300

    Update Uri1072.clj

--- TesteFuncionais/src/Uri1072.clj
+++ TesteFuncionais/src/Uri1072.clj
@@ -3,9 +3,13 @@
 (def entrada (read-string (read-line)))
 
 (defn listaEntrada [n]
-  (map (fn [p] (read-string (read-line))) (range 0 n))
+  (map (fn [*] (read-string (read-line))) (range 0 n))
   ) (def valores (listaEntrada entrada))
 
 (defn dentro [n]
   (and  (<= 10 n)  (<= n 20))
   )
+
+(println (count (filter dentro valores)) "in")
+(println (- (count valores) (count (filter dentro valores ) ) ) "out")
+
