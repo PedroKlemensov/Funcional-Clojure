b13bfa699b81ea6d9b10795dea2ae3f705915351
Pedroklemensov <80336621+PedroKlemensov@users.noreply.github.com>
2023-04-26 09:32:18 -0300

    a

---
+++ TesteFuncionais/src/Uri1131.clj
@@ -0,0 +1,46 @@
+(ns Uri1131)
+(require '[clojure.string :as str])
+
+(def entrada (list))
+(defn listaEntrada [*]
+  (def gols (str/split (read-line) #" "))
+  (def x (read-string (get gols 0)))
+  (def y (read-string (get gols 1)))
+
+  (def entrada (conj entrada (hash-map :Inter x :Gremio y)))
+  (println "Novo grenal (1-sim 2-nao)")
+  (def novo (read-string (read-line)))
+  (cond (== novo 1) (listaEntrada ())  )
+  )
+   (listaEntrada ())
+
+
+(defn gremio? [jogos]
+  (> (:Gremio jogos) (:Inter jogos))
+  )
+(defn inter? [jogos]
+  (< (:Gremio jogos) (:Inter jogos))
+  )
+(defn enpate? [jogos]
+  (== (:Gremio jogos) (:Inter jogos))
+  )
+
+(def vgremio (count (filter gremio? entrada)))
+
+(def vinter (count (filter inter? entrada)))
+
+(def empates (count (filter enpate? entrada)))
+
+(def ganhador  (cond
+                     (and (> empates vinter) (> empates vgremio)) (str "Nao houve vencedor")
+                     (> vgremio vinter) (str "Inter venceu mais")
+                     (< vgremio vinter) (str "Gremio venceu mais")
+                     ))
+(print (format
+           "%d grenais\nInter: %d\nGremio: %d\nEmpates: %d\n%s\n"
+           (count entrada)
+           vinter
+           vgremio
+           empates
+           ganhador
+           ))
