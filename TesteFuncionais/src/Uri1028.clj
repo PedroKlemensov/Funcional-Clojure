ad031365674eebbe7c25e5ac69bc66d89ee53211
Pedroklemensov <pedroklemensov@gmail.com>
2023-05-06 00:47:50 -0300

    Update Uri1028.clj

--- TesteFuncionais/src/Uri1028.clj
+++ TesteFuncionais/src/Uri1028.clj
@@ -2,13 +2,25 @@
 (require '[clojure.string :as str])
 
 
+
 (defn listaEntrada [n]
-  (map (fn [p]
-         ((str/split (read-line) #" "))
+  (map (fn [p] (mapv #(Integer/parseInt %) (str/split (read-line) #" "))
          )
-       (range 0 n))
-  )
+       (range n)))
 
 (def valores (listaEntrada (read-string (read-line))))
 
-(println valores)
+
+
+(defn MDC [a b]
+  (if (= b 0)
+    (println a)
+    (recur b (rem a b))))
+
+
+
+(defn chamada [reg]
+  (MDC (apply max reg) (apply min reg)))
+
+(doseq [v valores]
+  (chamada v))
